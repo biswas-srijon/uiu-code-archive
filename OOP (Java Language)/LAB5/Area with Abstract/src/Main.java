b1ba7129e3e2c024c909065231c33dfe60d26cd9
Srijon Biswas <isrijon10@gmail.com>
2024-08-09 22:38:31 +0600

    Update Main.java
    Edited with more user friendly code

--- OOP (Java Language)/LAB5/Area with Abstract/src/Main.java
+++ OOP (Java Language)/LAB5/Area with Abstract/src/Main.java
@@ -1,17 +1,38 @@
+package Abstract;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x,y,p,q,r;
 
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter value for Rectangle : ");
+        System.out.println("Enter 2 dimensions of Rectangle.");
+        System.out.print("X : ");
+        x=sc.nextDouble();
+        System.out.print("Y : ");
+        y=sc.nextDouble();
 
-        Shape rec = new Rectangle(10,20);
-        Shape tri = new Triangle(5,8);
-        Shape cir = new Circle(5);
+        System.out.println("Enter base and height of Triangle.");
+        System.out.print("P : ");
+        p=sc.nextDouble();
+        System.out.print("Q : ");
+        q=sc.nextDouble();
 
-        rec.printArea();
-        tri.printArea();
-        cir.printArea();
+        System.out.println("Enter radius of circle.");
+        System.out.print("r : ");
+        r=sc.nextDouble();
+        System.out.println("\n");
+
+        Shape sp;  // sp is the object variable for abstract class Shape
+
+        sp = new Rectangle(x,y);
+        sp.area();
+
+        sp = new Triangle(p,q);
+        sp.area();
+
+        sp = new Circle(r,r);
+        sp.area();
     }
 }
